4881a373826f4d52cefc6271e7677cda295465e9
YanbingJiang <yanbing.jiang@intel.com>
2022-01-21 09:36:18 +0800

    Update dataset to 3 RNN-T training datasets (#357)
    * Update dataset to 3 RNN-T training datasets
    
    In this commit, train-clean-360 and train-other-500 are added in model.
    These datasets need 500GB disk space to preprocess. It will take ~4 hours
    to run the entire 3 datasets for one epoch in BF16. You can terminate
    the training process by adding `num_steps` in
    models/language_modeling/pytorch/rnnt/training/cpu/train.sh.
    
    * Set NUM_STEPS outside of bash script
    
    * Add note that FP32 runs 100 steps

--- quickstart/language_modeling/pytorch/rnnt/training/cpu/training_multinode.sh
+++ quickstart/language_modeling/pytorch/rnnt/training/cpu/training_multinode.sh
@@ -33,13 +33,29 @@ if [ ! -d "${OUTPUT_DIR}" ]; then
   exit 1
 fi
 
+if [[ $1 == "avx-fp32" ]]; then
+    unset DNNL_MAX_CPU_ISA
+fi
+
 ARGS=""
 if [ "$1" == "bf16" ]; then
     ARGS="$ARGS bf16"
     echo "### running bf16 datatype"
-else
+elif [[ $1 == "fp32" || $1 == "avx-fp32" ]]; then
     ARGS="$ARGS fp32"
     echo "### running fp32 datatype"
+else
+    echo "The specified precision '${1}' is unsupported."
+    echo "Supported precisions are: fp32, avx-fp32, and bf16"
+    exit 1
 fi
 
-bash ${MODEL_DIR}/models/language_modeling/pytorch/rnnt/training/cpu/train.sh $ARGS
+PRECISION=$1
+
+torch_ccl_path=$(python -c "import torch; import torch_ccl; import os;  print(os.path.abspath(os.path.dirname(torch_ccl.__file__)))")
+source $torch_ccl_path/env/setvars.sh
+if [[ ! -z "${NUM_STEPS}" ]]; then
+    NUM_STEPS=$NUM_STEPS bash ${MODEL_DIR}/models/language_modeling/pytorch/rnnt/training/cpu/train_multinode.sh $ARGS 2>&1 | tee -a ${OUTPUT_DIR}/rnnt-training-distributed-${PRECISION}.log
+else
+    bash ${MODEL_DIR}/models/language_modeling/pytorch/rnnt/training/cpu/train_multinode.sh $ARGS 2>&1 | tee -a ${OUTPUT_DIR}/rnnt-training-distributed-${PRECISION}.log
+fi

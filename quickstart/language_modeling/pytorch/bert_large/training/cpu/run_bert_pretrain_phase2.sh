3de6f692cd18fd963bf8601f85b73321f1f58369
liangan1 <liangang.zhang@intel.com>
2022-01-27 10:17:56 +0800

    update bs and use ipex Lamb (#382)

--- quickstart/language_modeling/pytorch/bert_large/training/cpu/run_bert_pretrain_phase2.sh
+++ quickstart/language_modeling/pytorch/bert_large/training/cpu/run_bert_pretrain_phase2.sh
@@ -25,7 +25,7 @@ if [[ "$1" == "bf16" ]]
 then
     ARGS="$ARGS --bf16"
     precision=bf16
-    batch_size=56
+    batch_size=128
     echo "### running bf16 mode"
 elif [[ $1 == "fp32" ]]; then
     echo "### running FP32 mode"

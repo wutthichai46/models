96c2c6a58e47cb0ca5f1c38b5e5c03ed69a18898
jiayisunx <jiayi.sun@intel.com>
2022-03-02 13:56:40 +0800

    use multi-instances for maskrcnn training (#445)

--- quickstart/object_detection/pytorch/maskrcnn/training/cpu/train_throughput_baremetal.sh
+++ quickstart/object_detection/pytorch/maskrcnn/training/cpu/train_throughput_baremetal.sh
@@ -66,8 +66,7 @@ rm -rf ${OUTPUT_DIR}/train_throughput_log*
 
 python -m intel_extension_for_pytorch.cpu.launch \
     --enable_jemalloc \
-    --ninstances 1 \
-    --ncore_per_instance ${CORES_PER_INSTANCE} \
+    --throughput_mode \
     ${MODEL_DIR}/models/object_detection/pytorch/maskrcnn/maskrcnn-benchmark/tools/train_net.py \
     $ARGS \
     --iter-warmup 10 \
